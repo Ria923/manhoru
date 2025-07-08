999b45079fb418a52d21a53ee0ea0aa5419b6a9c
Ria <pudding9311519@gmail.com>
2025-07-08 10:56:56 +0900

    Add onboarding and delete explore

--- monhoru/app/_layout.tsx
+++ monhoru/app/_layout.tsx
@@ -3,33 +3,26 @@ import {
   DefaultTheme,
   ThemeProvider,
 } from "@react-navigation/native";
-import { useFonts } from "expo-font";
-import { Stack } from "expo-router";
+import { Stack, Redirect } from "expo-router";
 import { StatusBar } from "expo-status-bar";
-import "react-native-reanimated";
 import React from "react";
 
 import { useColorScheme } from "@/hooks/useColorScheme";
 
 export default function RootLayout() {
   const colorScheme = useColorScheme();
-  const [loaded] = useFonts({
-    SpaceMono: require("../assets/fonts/SpaceMono-Regular.ttf"),
-  });
-
-  if (!loaded) {
-    // Async font loading only occurs in development.
-    return null;
-  }
 
   return (
     <ThemeProvider value={colorScheme === "dark" ? DarkTheme : DefaultTheme}>
       <Stack>
+        <Stack.Screen name="onboarding" options={{ headerShown: false }} />
+        <Stack.Screen name="userlogin" options={{ headerShown: false }} />
         <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
         <Stack.Screen name="profile_edit" options={{ headerShown: true }} />
         <Stack.Screen name="terms" options={{ headerShown: true }} />
         <Stack.Screen name="+not-found" options={{ headerShown: false }} />
       </Stack>
+      <Redirect href="/onboarding" />
       <StatusBar style="auto" />
     </ThemeProvider>
   );
