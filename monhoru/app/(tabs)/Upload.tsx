31913f850106add6412d956f89724fcf8e22e8aa
Fujita <24aw0133@jec.ac.jp>
2025-07-09 21:48:18 +0900

    fix: Uploadの場所表示文書を修正

--- monhoru/app/(tabs)/Upload.tsx
+++ monhoru/app/(tabs)/Upload.tsx
@@ -74,8 +74,7 @@ export default function GalleryScreen() {
 
           if (geocode.length > 0) {
             const place = geocode[0];
-            const fullAddress = `${place.region ?? ""}${place.city ?? ""}${
-              place.street ?? ""
+            const fullAddress = `${place.region ?? ""}${place.city ?? ""}
             }`;
             setAddress(fullAddress || "住所情報が見つかりませんでした");
           } else {
